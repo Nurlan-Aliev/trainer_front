5ba8083a66ad0165950439bf28c28499afe1a8fd
Nurlan-Aliev <nualiev612@gmail.com>
2026-02-09 02:02:38 +0400

    fix translate bug

--- src/component/Words/translateCard/translateCard.jsx
+++ src/component/Words/translateCard/translateCard.jsx
@@ -10,14 +10,11 @@ export function TranslateCard({word, nextWord, url, token, countFunc}) {
     const [inputValue, setInputValue] = useState("");
     const [chang, setChange] = useState(false);
     const [correctAnswer, setCorrectAnswer] = useState(null);
-    const questionLanguage = url === '/translate' ? 'word_en' : `word_${i18n.language}`;
-    const answerLanguage = url === '/translate' ? `word_${i18n.language}` : 'word_en';
 
     const handleSubmit = async () =>{
         const response = await postRequest(url,{
-            "user_answer": inputValue,
-            "word_id": word.word_id,
-            "language": `word_${i18n.language}`
+            "id": word.word_id,
+            "user_answer": inputValue
         },token )
         setCorrectAnswer(response.detail);
         setChange(true);
@@ -33,26 +30,26 @@ export function TranslateCard({word, nextWord, url, token, countFunc}) {
 
     return (
         <div className={style.container}>
-            <h3>{word.question[questionLanguage]}</h3>
+            <h3>{word.question}</h3>
             <div className={style.options}>
-                {word.options.map((i) => {
+                {word.option.map((i) => {
                     let btnClass = "btn btn-primary m-1";
                     if (chang) {
-                        if (i[answerLanguage] === correctAnswer) btnClass = "btn btn-success m-1";
-                        else if (i[answerLanguage] === inputValue && i !== correctAnswer)
+                        if (i === correctAnswer) btnClass = "btn btn-success m-1";
+                        else if (i === inputValue && i !== correctAnswer)
                             btnClass = "btn btn-danger m-1";
                     }
-                    else if (i[answerLanguage] === inputValue) {
+                    else if (i === inputValue) {
                         btnClass = "btn btn-warning m-1";
                     }
                     return (
                         <button
                             className={btnClass}
                             key={i}
-                            onClick={() => setInputValue(i[answerLanguage])}
+                            onClick={() => setInputValue(i)}
                             disabled={!!correctAnswer}
                         >
-                            {i[answerLanguage]}
+                            {i}
                         </button>
                     );
                 })}
